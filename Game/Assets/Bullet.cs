1c7a2c62dad58ab3d3cae8452be322af9e63ad89
FrogInMySock <Rman434@gmail.com>
2023-01-01 20:33:50 +0000

    Gun model made
    Gun Script/ Bullet Script added

---
+++ Game/Assets/Bullet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float life = 3;
+
+    void Awake()
+    {
+        Destroy(gameObject, life);
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+    }
+}
---
+++ Game/Assets/Gun.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Gun : MonoBehaviour
+{
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10;
+    public playerController playerScript;
+ 
+    void Update()
+    {
+        Gun1();
+    }
+
+    void Gun1()
+    {
+         
+        if(Input.GetMouseButtonDown(0))
+            {
+                var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+                bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+            }
+    }
+
+}
